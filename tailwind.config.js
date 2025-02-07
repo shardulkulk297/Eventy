c2ba52ba6e91804d6c696c4f3732d9f0d470f6fa
sahilmendgule03@gmail.com <shardulkulk297@gmail.com>
2025-02-07 22:23:04 +0530

    Fixed the error of shadcN

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+// tailwind.config.js
+
+import animate from 'tailwindcss-animate';
+
+export default {
+  darkMode: ["class"],
+  content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+      },
+    },
+  },
+  plugins: [animate],
+};
