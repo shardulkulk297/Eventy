35d42b2ca04212da52ee0c1e4c9b65a0d501e75c
sahilmendgule03@gmail.com <shardulkulk297@gmail.com>
2025-02-09 00:35:32 +0530

    Fixed

--- src/components/ui/ui/LoginForm.jsx
+++ src/components/ui/ui/LoginForm.jsx
@@ -14,7 +14,7 @@ import {
 } from "./select";
 
 export function LoginForm() {
-  const [loginData, setLoginData] = useState({ username: "", password: "" });
+  const [loginData, setLoginData] = useState({ Email: "", password: "" });
   const [registerData, setRegisterData] = useState({
     name: "",
     displayName: "",
