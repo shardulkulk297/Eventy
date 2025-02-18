d94752dff7e64e154a7625f32e51d3c614c684be
Shardul <shardulkulk297@gmail.com>
2025-02-18 10:07:44 +0530

    Done with the layout of sidebar

--- Frontend/src/components/AppSidebar.jsx
+++ Frontend/src/components/AppSidebar.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
-import { Sidebar, SidebarContent, SidebarFooter, SidebarFooter, SidebarGroup, SidebarHeader } from './ui/sidebar'
+import { Sidebar, SidebarContent, SidebarFooter, SidebarGroup, SidebarHeader } from './ui/sidebar'
 const AppSidebar = () => {
   return (
     <Sidebar>
         <SidebarHeader />
         <SidebarContent>
-            <SidebarGroup />
-            <SidebarGroup />
+            <SidebarGroup>
+                Pokemon
+            </SidebarGroup>
 
         </SidebarContent>
         <SidebarFooter />
