c91dfb52eb5703d742f9935466386b77fca4e5a9
Shardul <shardulkulk297@gmail.com>
2025-02-19 11:18:37 +0530

    Added content in sidebar

--- Frontend/src/components/AppSidebar.jsx
+++ Frontend/src/components/AppSidebar.jsx
@@ -1,21 +1,63 @@
 import React from 'react'
-import { Sidebar, SidebarContent, SidebarFooter, SidebarGroup, SidebarHeader, SidebarGroupLabel, SidebarGroupContent } from './ui/sidebar'
+import { Sidebar, SidebarContent, SidebarFooter, SidebarGroup, SidebarHeader, SidebarGroupLabel, SidebarGroupContent, SidebarMenu, SidebarMenuItem, SidebarMenuButton } from './ui/sidebar'
+import { Home, EarthIcon, FunctionSquare, ArrowBigUpIcon } from 'lucide-react'
 const AppSidebar = () => {
-  return (
-    <Sidebar>
-        <SidebarHeader>Eventy</SidebarHeader>
-        <SidebarContent>
-            <SidebarGroup>
-                <SidebarGroupLabel>Navigate</SidebarGroupLabel>
-                <SidebarGroupContent>
-                  
-                </SidebarGroupContent>
-            </SidebarGroup>
 
-        </SidebarContent>
-        <SidebarFooter />
-    </Sidebar>
-  )
+  const items = [
+    {
+      title: "Home",
+      url: '/posts',
+      icon: Home
+    },
+
+    {
+      title: "Hackathons",
+      url: '/hack',
+      icon: EarthIcon
+    },
+
+    {
+      title: "Upcoming",
+      url: '/upcoming',
+      icon: ArrowBigUpIcon
+    }
+  ]
+
+return (
+  <Sidebar>
+    <SidebarHeader>Eventy</SidebarHeader>
+    <SidebarContent>
+      <SidebarGroup>
+        <SidebarGroupLabel>Navigate</SidebarGroupLabel>
+        <SidebarGroupContent>
+          <SidebarMenu>
+
+            {
+              items.map((item)=>{
+                <SidebarMenuItem key={item.title}>
+
+                  <SidebarMenuButton asChild>
+                    
+                  <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+
+                  </a>
+                  </SidebarMenuButton>
+
+                </SidebarMenuItem>
+              })
+            }
+
+          </SidebarMenu>
+
+        </SidebarGroupContent>
+      </SidebarGroup>
+
+    </SidebarContent>
+    <SidebarFooter />
+  </Sidebar>
+)
 }
 
 export default AppSidebar
